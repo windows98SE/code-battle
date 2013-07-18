88252c35e7b2e3cb496f20eb3f67abc1888c61dc
windows98SE <nolist@gmail.com>
2013-07-19 01:13:33 +0700

    just another perl shoter
    shot but fast

--- q2/best_script.pl
+++ q2/best_script.pl
@@ -1,22 +1,2 @@
 #!/usr/bin/perl
-my %numbers = readfile('numbers.txt');
-
-open F, ">", 'run_result.txt';
-#elegant but actually 30% slower :P
-#print F "\"$_\", $numbers{$_}\n" for (keys %numbers);
-while (my ($k,$v)=each %numbers){
-  print F "\"$k\", $v\n";
-}
-close F;
-
-
-sub readfile{
-  my %var;
-  open F, "<", $_[0];
-  while (my $line = <F>){
-    $line =~ s/\r?\n//ig;
-    ++$var{$line};
-  }
-  close(F);
-  return %var;
-}
+open F,"<numbers.txt";while(<F>){$_=~s/\r?\n//ig;++$h{$_};}close F;open F,">run_result.txt";while(($k,$v)=each%h){print F "\"$k\", $v\n";}close F;
