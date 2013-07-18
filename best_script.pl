e7e81ae304e3c9449f266ffc5532366a9c9f400e
windows98SE <nolist@gmail.com>
2013-07-18 23:00:34 +0700

    by windows98SE fb.com/windows98SE
    just another perl hacker. :p

---
+++ best_script.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+my @array = ('0000'..'9999');
+my %numbers = readfile('numbers.txt');
+my @run_result = grep {!$numbers{$_}} @array; 
+
+open F, ">", 'run_result.txt';
+print F join "\n", @run_result;
+close F;
+
+
+sub readfile{
+  my @var;
+  open F, "<", $_[0];
+  while (my $line = <F>){
+  $line =~ s/\r?\n//ig;
+	$line =~ s/^\s+|\s+//ig;
+	push(@var, $line) if($line);
+  }
+  close(F);
+  return map {$_=>1} @var;
+}
