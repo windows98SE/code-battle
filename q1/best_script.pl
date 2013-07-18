43611b693aa63fa88b1b289106eaf627e229f223
windows98SE <nolist@gmail.com>
2013-07-19 01:14:59 +0700

    just another perl shoter
    shot but fast

--- q1/best_script.pl
+++ q1/best_script.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl
-open F,"<",'numbers.txt';while(<F>){$_=~s/\r?\n//ig;$h{$_}=1;}close F;@r=grep{!$h{$_}}'0000'..'9999';open F,">",'run_result.txt';print F join"\n",@r;close F;
+open F,"<numbers.txt";while(<F>){$_=~s/\r?\n//ig;$h{$_}=1;}close F;@r=grep{!$h{$_}}'0000'..'9999';open F,">run_result.txt";print F join"\n",@r;close F;
