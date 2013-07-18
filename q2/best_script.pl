30cf6ced7f30e2273eeee03292978280cc569904
windows98SE <nolist@gmail.com>
2013-07-19 02:12:03 +0700

    just another perl shoter
    #3

--- q2/best_script.pl
+++ q2/best_script.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl
-open F,"<numbers.txt";while(<F>){$_=~s/\r?\n//ig;++$h{$_};}close F;open F,">run_result.txt";while(($k,$v)=each%h){print F "\"$k\", $v\n";}close F;
+open F,'<numbers.txt';while(<F>){chomp$_;++$h{$_};}close F;open F,'>run_result.txt';while(($k,$v)=each%h){print F "\"$k\", $v\n";}close F;
