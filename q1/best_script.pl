80cbd1fa7c4f1dee530e6b9e5780d50ad5c4cd47
windows98SE <nolist@gmail.com>
2013-07-19 02:23:15 +0700

    just another perl shoter
    #4

--- q1/best_script.pl
+++ q1/best_script.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl
-open F,'<numbers.txt';while(<F>){chomp$_;$h{$_}=1;}close F;open F,'>run_result.txt';print F join"\n",grep{!$h{$_}}'0000'..'9999';close F;
+open F,'<numbers.txt';while(<F>){chomp$_;$h{$_}=1;}open F,'>run_result.txt';print F join"\n",grep{!$h{$_}}'0000'..'9999';
