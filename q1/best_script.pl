ef91af8f08c43d3fa174982922f89ead55d3c4fa
windows98SE <nolist@gmail.com>
2013-07-19 17:17:02 +0700

    just another perl hacker
    update comment
    
    #win : type numbers.txt | perl best_script.pl>result_run.txt
    #nix : cat numbers.txt | ./best_script.pl>result_run.txt

--- q1/best_script.pl
+++ q1/best_script.pl
@@ -1,3 +1,4 @@
 #!/usr/bin/perl
-#cat numbers.txt | ./best_script.pl > result_run.txt
+#win : type numbers.txt | perl best_script.pl>result_run.txt
+#nix : cat numbers.txt | ./best_script.pl>result_run.txt
 open I,"-";map{$n{$_}=chomp$_}<>;print join"\n",grep{!$n{$_}}'0'x4..'9'x4;
