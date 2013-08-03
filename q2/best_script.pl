5e860037fb1094badeae50cc01e07640091b34c5
windows98SE <nolist@gmail.com>
2013-08-03 16:18:35 +0700

    change count str to int
    #_#

--- q2/best_script.pl
+++ q2/best_script.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 #win : type numbers.txt | perl best_script.pl>result_run.txt
 #nix : cat numbers.txt | ./best_script.pl>result_run.txt
-open I,"-";map{chomp$_=>$n{$_}++}<>;print "\"$_\", $n{$_}\n"for(keys%n);
+open I,"-";map{chomp$_=>$n{int($_)}++}<>;print "\"$_\", $n{$_}\n"for(keys%n);
