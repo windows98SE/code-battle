c6565b826842c198230989242f9fc6a36011611e
windows98SE <nolist@gmail.com>
2013-08-05 01:10:56 +0700

    Update best_script.pl

--- q2/best_script.pl
+++ q2/best_script.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 #win : type numbers.txt | perl best_script.pl>result_run.txt
 #nix : cat numbers.txt | ./best_script.pl>result_run.txt
-open I,"-";map{chomp$_=>++$n{"$_"}}<>;for(sort keys(%n)){print"\"$_\", $n{$_}\n"if($n{$_}>1);};
+open I,"-";map{chomp$_=>++$n{$_}}<>;for(sort keys(%n)){print"\"$_\", $n{$_}\n"if($n{$_}>1);};
