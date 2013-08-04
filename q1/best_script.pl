e814bfdc5de5d4c6e289c26a299362edd8a63c1c
windows98SE <nolist@gmail.com>
2013-08-05 01:28:23 +0700

    Update best_script.pl

--- q1/best_script.pl
+++ q1/best_script.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
-#win : type numbers.txt | perl best_script.pl>result_run.txt
-#nix : cat numbers.txt | ./best_script.pl>result_run.txt
+#win : type numbers.txt | perl best_script.pl>run_result.txt
+#nix : cat numbers.txt | ./best_script.pl>run_result.txt
 open I,"-";map{$n{$_}=chomp$_}<>;print join"\n",grep{!$n{$_}}'0'x4..'9'x4;
