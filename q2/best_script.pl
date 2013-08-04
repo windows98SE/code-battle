f8285874e39046981987f3325f1cb46b33e8223d
windows98SE <nolist@gmail.com>
2013-08-05 01:30:02 +0700

    Update best_script.pl

--- q2/best_script.pl
+++ q2/best_script.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 #win : type numbers.txt | perl best_script.pl>run_result.txt
 #nix : cat numbers.txt | ./best_script.pl>run_result.txt
-open I,"-";map{chomp$_=>++$n{$_}}<>;for(sort keys(%n)){print"\"$_\", $n{$_}\n"if($n{$_}>1);};
+open I,"-";map{chomp$_=>++$n{$_}}<>;for(keys(%n){print"\"$_\", $n{$_}\n"if($n{$_}>1);};
