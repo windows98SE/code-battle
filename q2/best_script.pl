46a16af467e90888e19408aee292880984fe48b0
windows98SE <nolist@gmail.com>
2013-08-05 01:09:34 +0700

    print $n{$_} > 1 only
    commit

--- q2/best_script.pl
+++ q2/best_script.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 #win : type numbers.txt | perl best_script.pl>result_run.txt
 #nix : cat numbers.txt | ./best_script.pl>result_run.txt
-open I,"-";map{chomp$_=>$n{int($_)}++}<>;print "\"$_\", $n{$_}\n"for(keys%n);
+open I,"-";map{chomp$_=>++$n{"$_"}}<>;for(sort keys(%n)){print"\"$_\", $n{$_}\n"if($n{$_}>1);};
